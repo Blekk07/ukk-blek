1ecfc5761f99bc9734736a31e95e01626fabc2ae
Blekk07 <dimasatrya12@gmail.com>
2025-11-11 04:07:12 -0500

    Commit perubahan sebelum mengubah author

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,7 +1,7 @@
 <li class="pc-item">
     <a href="{{ route('admin.verifikasi') }}" class="pc-link">
         <span class="pc-micon"><i class="ti ti-file-check"></i>
-        </span><span class="pc-mtext">Verifikasi Berkas</span></a>
+        </span><span class="pc-mtext">Data Penduduk</span></a>
 </li>
 <li class="pc-item"><a href="{{ route('admin.seleksi') }}" class="pc-link">
         <span class="pc-micon"><i class="ti ti-list-check"></i></span>
